d70113cffefc3706d6d9e361284a7953d598b932
Shinazmohamed <115807856+Shinazmohamed@users.noreply.github.com>
2023-12-01 17:15:17 +0400

    changes

---
+++ IdentityApplication/ViewComponents/NavigationMenuViewComponent.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace IdentityApplication.ViewComponents
+{
+    public class NavigationMenuViewComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
